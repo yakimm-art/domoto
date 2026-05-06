b7b0d9783cd602c7393e69765a1cadbf4aaf1e4a
Joanne R. Georfo <georfo.joanne05@gmail.com>
2026-05-06 11:59:45 +0800

    Features: Improved LoginView

--- Domoto/Views/LoginView.xaml.cs
+++ Domoto/Views/LoginView.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using Domoto.ViewModels;
 
 namespace Domoto.Views
@@ -9,27 +10,40 @@ namespace Domoto.Views
         public LoginView()
         {
             InitializeComponent();
+            
+            // Focus the username field when the view loads
+            this.Loaded += (s, e) => 
+            {
+                if (UsernameBox != null) Keyboard.Focus(UsernameBox);
+            };
         }
 
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            var vm = DataContext as LoginViewModel;
+            // Using a traditional 'as' cast instead of pattern matching
+            LoginViewModel vm = DataContext as LoginViewModel;
             if (vm != null)
+            {
                 vm.Password = ((PasswordBox)sender).Password;
+            }
         }
 
         private void RegPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            var vm = DataContext as LoginViewModel;
+            LoginViewModel vm = DataContext as LoginViewModel;
             if (vm != null)
+            {
                 vm.RegisterPassword = ((PasswordBox)sender).Password;
+            }
         }
 
         private void RegConfirmPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            var vm = DataContext as LoginViewModel;
+            LoginViewModel vm = DataContext as LoginViewModel;
             if (vm != null)
+            {
                 vm.RegisterConfirmPassword = ((PasswordBox)sender).Password;
+            }
         }
     }
 }
