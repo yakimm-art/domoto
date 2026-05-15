a1507e762263d2e0ca14f99f4de419c00a2d09fd
igotbarbiekyle <barbiekylei@gmail.com>
2026-05-15 22:14:22 +0800

    feat: show/hide password toggle + Enter key to sign in

--- Domoto/Views/LoginView.xaml.cs
+++ Domoto/Views/LoginView.xaml.cs
@@ -7,11 +7,12 @@ namespace Domoto.Views
 {
     public partial class LoginView : UserControl
     {
+        private bool _passwordVisible = false;
+
         public LoginView()
         {
             InitializeComponent();
-            
-            this.Loaded += (s, e) => 
+            this.Loaded += (s, e) =>
             {
                 if (UsernameBox != null) Keyboard.Focus(UsernameBox);
             };
@@ -19,29 +20,66 @@ namespace Domoto.Views
 
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            LoginViewModel vm = DataContext as LoginViewModel;
+            var vm = DataContext as LoginViewModel;
             if (vm != null)
-            {
                 vm.Password = ((PasswordBox)sender).Password;
+
+            // Keep the plain-text box in sync if visible
+            if (_passwordVisible)
+                PasswordVisible.Text = ((PasswordBox)sender).Password;
+        }
+
+        // Toggle show/hide password
+        private void TogglePassword_Click(object sender, RoutedEventArgs e)
+        {
+            _passwordVisible = !_passwordVisible;
+
+            if (_passwordVisible)
+            {
+                PasswordVisible.Text = PasswordBox.Password;
+                PasswordBox.Visibility = Visibility.Collapsed;
+                PasswordVisible.Visibility = Visibility.Visible;
+                EyeIcon.Text = "🙈";
             }
+            else
+            {
+                PasswordBox.Password = PasswordVisible.Text;
+                PasswordVisible.Visibility = Visibility.Collapsed;
+                PasswordBox.Visibility = Visibility.Visible;
+                EyeIcon.Text = "👁";
+            }
+        }
+
+        // Press Enter on password box to sign in
+        private void PasswordBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var vm = DataContext as LoginViewModel;
+                if (vm != null && vm.LoginCommand.CanExecute(null))
+                    vm.LoginCommand.Execute(null);
+            }
+        }
+
+        // Press Enter on username box to move focus to password
+        private void UsernameBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                Keyboard.Focus(PasswordBox);
         }
 
         private void RegPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            LoginViewModel vm = DataContext as LoginViewModel;
+            var vm = DataContext as LoginViewModel;
             if (vm != null)
-            {
                 vm.RegisterPassword = ((PasswordBox)sender).Password;
-            }
         }
 
         private void RegConfirmPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            LoginViewModel vm = DataContext as LoginViewModel;
+            var vm = DataContext as LoginViewModel;
             if (vm != null)
-            {
                 vm.RegisterConfirmPassword = ((PasswordBox)sender).Password;
-            }
         }
     }
 }
